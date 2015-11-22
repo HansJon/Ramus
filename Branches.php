357e2581dd020afbf2c52a4b1d6dd9b413c64727
Maximpop <rudkovpop@gmail.com>
2015-11-22 14:40:29 +0000

    Layout for branches

---
+++ Branches.php
@@ -0,0 +1,77 @@
+<html>
+	<head>
+		<title>Branches</title>
+	 	<meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	    <link rel="stylesheet" media="screen,projection" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.2/css/materialize.min.css">
+	    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	    <link href="https://code.jquery.com/jquery-2.1.4.min.js">
+	    <link rel="stylesheet" href="CSS/branches.css" media="screen" charset="utf-8">
+	</head>
+	<body>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.2/js/materialize.min.js"></script>	
+				<div class="image"></div>
+				<nav>
+				    <div class="nav-wrapper">
+				      <img class="ramus" src="Media\Images\Icon\Ramus-Icon.ico">
+				      <ul id="nav-mobile" class="right hide-on-med-and-down">
+				        <li><a href="sass.html">User_ID</a></li>
+				        <li><a href="badges.html">something</a></li>
+				        <li><a href="collapsible.html">Log out</a></li>
+				      </ul>
+				    </div>
+			  </nav>
+			  <div class="row">
+			  	<div class="col s12 m6 l3   "><div class=" colorbranch "><center><i class="large material-icons">code</i></center><h3>Computer Science</h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			  	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			  	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			  	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			  	cillum dolore eu fugiat nulla pariat
+			  	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div></div>
+			  	<div class="col s12 m6 l3   "><div class=" colorbranch"><center><i class="large material-icons">code</i></center><h3>Entertainment</h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			  	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			  	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			  	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			  	cillum dolore eu fugiat nulla pariat
+			  	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div></div>
+			  	<div class="col s12 m6 l3   "><div class=" colorbranch"><center><i class="large material-icons">code</i></center><h3>Health</h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			  	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			  	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			  	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			  	cillum dolore eu fugiat nulla pariat
+			  	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div></div>
+			  	<div class="col s12 m6 l3   "><div class="  colorbranch"><center><i class="large material-icons">code</i></center><h3>Parenting</h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			  	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			  	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			  	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			  	cillum dolore eu fugiat nulla pariat
+			  	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div></div>
+			  </div>
+			  <div class="row">
+			  	<div class="col s12 m6 l3   "><div class=" colorbranch"><center><i class="large material-icons">code</i></center><h3>Humor</h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			  	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			  	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			  	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			  	cillum dolore eu fugiat nulla pariat
+			  	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div></div>
+			  	<div class="col s12 m6 l3  "><div class=" colorbranch"><center><i class="large material-icons">code</i></center><h3>Business</h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			  	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			  	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			  	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			  	cillum dolore eu fugiat nulla pariat
+			  	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div></div>
+			  	<div class="col s12 m6 l3   "><div class=" colorbranch"><center><i class="large material-icons">code</i></center><h3>Motivational</h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			  	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			  	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			  	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			  	cillum dolore eu fugiat nulla pariat
+			  	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div></div>
+			  	<div class="col s12 m6 l3   "><div class=" colorbranch"><center><i class="large material-icons">code</i></center><h3>Education</h3><p>Lor3m ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			  	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			  	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			  	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			  	cillum dolore eu fugiat nulla pariat
+			  	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div></div>
+			  </div>				
+	</body>
+</html>
