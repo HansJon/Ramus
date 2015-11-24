64cc4dfa291a9613fe3cb011ba1fbaeb763c4ea1
Hans Jón <HJIvars@hotmail.com>
2015-11-24 19:47:55 +0000

    Branches from DB, independent navbar and fixed bg
    + Branches from database: every tile on the branches site is from the
    database.
    + External NavBar: Seperated navbar from branches and made it
    independent across sites and is now sited in "includes/navbar.php"
    + Fixed Background: Changed background code entirely to a much simpler
    method using default CSS3 properties and no go-arounds.

---
+++ twigs/compsci/subsidium/index.php
@@ -0,0 +1,34 @@
+<html>
+	<head>
+		<title>Branches</title>
+	 	<meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	    <link rel="stylesheet" media="screen,projection" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.2/css/materialize.min.css">
+	    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	    <link href="https://code.jquery.com/jquery-2.1.4.min.js">
+	    <link rel="stylesheet" href="/CSS/subsidium.css" media="screen" charset="utf-8">
+	
+	</head>
+	<body>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.2/js/materialize.min.js"></script>	
+				<div class="image"></div>
+				<nav>
+				    <div class="nav-wrapper">
+				      <img class="ramus" src="/Media/Images/Icons/Ramus.ico">
+				      <ul id="nav-mobile" class="right hide-on-med-and-down">
+				        <li><a href="badges.html">something</a></li>
+				        <li><a href="user.php.html">User_ID</a></li>
+				        <li><a href="/logout.php">Log out</a></li>
+				      </ul>
+				    </div>
+			  </nav>
+			  
+			  <div class="row">
+			  <div Class="col s12 m6 White"><div class="colorbranch"><h3>Maxim Rudkov</h3></div></div>
+			  <div Class="col s12 m6 white"><div class="colorbranch"><h3>Maxim Rudkov</h3></div></div>
+			  </div>
+			  <div class="row">
+			  <div class="col s12 white"><div class="colorbranch"><h3>Maxim Rudkov</h3></div></div>
+			  </div>
+	</body>
+</html>
